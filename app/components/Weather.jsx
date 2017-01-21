1e216c36fd9a53f7a9657483b16b926405890190
David Whang <whangsajang@gmail.com>
2017-01-20 17:37:57 -0800

    Added Loading Text

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -6,29 +6,41 @@ var openWeatherMap = require('openWeatherMap');
 var Weather = React.createClass({
     getInitialState: function (){
         return{
-            location: 'Miami',
-            temp: 88
+          isLoading: false
         }
     },
     handleSearch: function(location){
         var that = this;
+
+        this.setState({isLoading: true});
+
         openWeatherMap.getTemp(location).then(function(temp){
             that.setState({
                 location: location,
-                temp: temp
+                temp: temp,
+                isLoading: false
             })
         }), function(errorMessage){
             alert(errorMessage);
+            that.setState({isLoading: false});
         }
   
     },
     render: function(){
-        var {temp, location} = this.state;
+        var {isLoading, temp, location} = this.state;
+
+        function renderMessage(){
+            if(isLoading){
+                <h3>Fetching weather...</h3>;
+            }else if (temp && location){
+                return <WeatherMessage temp={temp} location={location}/>
+            }
+        }
         return(
             <div>
                 <h3>Weather Component</h3>
                 <WeatherForm onSearch={this.handleSearch} />
-                <WeatherMessage temp={temp} location={location} />
+                {renderMessage()}
             </div>    
         )
     }
--- public/bundle.js
+++ public/bundle.js
@@ -25503,26 +25503,43 @@
 
 	    getInitialState: function getInitialState() {
 	        return {
-	            location: 'Miami',
-	            temp: 88
+	            isLoading: false
 	        };
 	    },
 	    handleSearch: function handleSearch(location) {
 	        var that = this;
+
+	        this.setState({ isLoading: true });
+
 	        openWeatherMap.getTemp(location).then(function (temp) {
 	            that.setState({
 	                location: location,
-	                temp: temp
+	                temp: temp,
+	                isLoading: false
 	            });
 	        }), function (errorMessage) {
 	            alert(errorMessage);
+	            that.setState({ isLoading: false });
 	        };
 	    },
 	    render: function render() {
 	        var _state = this.state,
+	            isLoading = _state.isLoading,
 	            temp = _state.temp,
 	            location = _state.location;
 
+
+	        function renderMessage() {
+	            if (isLoading) {
+	                React.createElement(
+	                    'h3',
+	                    null,
+	                    'Fetching weather...'
+	                );
+	            } else if (temp && location) {
+	                return React.createElement(WeatherMessage, { temp: temp, location: location });
+	            }
+	        }
 	        return React.createElement(
 	            'div',
 	            null,
@@ -25532,7 +25549,7 @@
 	                'Weather Component'
 	            ),
 	            React.createElement(WeatherForm, { onSearch: this.handleSearch }),
-	            React.createElement(WeatherMessage, { temp: temp, location: location })
+	            renderMessage()
 	        );
 	    }
 	});
